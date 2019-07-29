ef8df204b95b33c9f6ffcbc6c533c8fe43636d66
sherry.chu33 <sherry.chu33@titansoft.com.sg>
2019-07-29 14:45:07 +0800

    使用Autofac，在mainfunction使用，有些步驟，目的是為了讓參數可以不用像版二那樣寫一堆窩~xDD

--- Autofac_pratice/Program.cs
+++ Autofac_pratice/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autofac;
 
 namespace Autofac_pratice
 {
@@ -41,11 +42,13 @@ namespace Autofac_pratice
 
     internal class Light
     {
+        private readonly IButton _circleButton;
         private readonly IButton _lightButton;
 
-        public Light(IButton lightButton)
+        public Light(IButton lightButton, IButton circleButton)
         {
             _lightButton = lightButton;
+            _circleButton = circleButton;
         }
 
         internal void Pull()
@@ -63,9 +66,38 @@ namespace Autofac_pratice
     {
         private static void Main(string[] args)
         {
-            var newLight = new Light(new Button());
-            newLight.Pull();//then turn on
-            newLight.PullTwice();//then turn off
+            //這是版本三，使用autofac
+
+            //(步驟1:建立ContainerBuilder)
+            ContainerBuilder builder = new ContainerBuilder();
+
+            //(步驟2:註冊型別)
+            builder.RegisterType<DecideButton>().As<IButton>();
+            builder.RegisterType<Light>();//key!!!也要註冊!
+
+            //(步驟3:建立容器)
+            var container = builder.Build();
+
+            //(步驟4:使用容器取得所需物件)
+            container.Resolve<Light>().Pull();
+            container.Resolve<Light>().PullTwice();
+
+            var light2 = container.Resolve<Light>();
+            light2.PullTwice();
+            Console.ReadKey();
+
+            ////PS:覺得奇怪xDD，雖然沒有作用，卻也沒出錯////
+            //IButton button = container.Resolve<IButton>();
+            //button.Off();
+
+            //這是版本二，我的燈要有兩個按鈕(參數)，每生出幾盞燈，燈就要有那些參數
+            //var button = new Button();
+            //var light = new Light(button,button);
+            //var light1 = new Light(button,button);
+
+            //這是版本一，要使用new
+            //newLight.Pull();//then turn on
+            //newLight.PullTwice();//then turn off
         }
     }
 }
